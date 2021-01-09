a75a3f7bae6c383166f27a8c53d037e9490774a0
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-01-09 22:35:44 +0200

    styleing...

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var dubai = new Store('Dubai', 38, 11, 3.7);
 var lima = new Store('Lima', 2, 16, 4.6);
 var paris = new Store('Paris', 20, 38, 2.3);
 var newinput = new Store()
-var citynamearray = [seattle, tokyoo, dubai, lima, paris,'Total'];
+var citynamearray = [seattle, tokyoo, dubai, lima, paris, 'Total'];
 var hourarray = ['Location', '6 AM :', '7 AM :', '8 AM :', '9 AM :', '10 AM :', '11 AM :', '12 PM :', '1 PM :', '2 PM :', '3 PM :', '4 PM :', '5 PM :', '6 PM :', '7 PM :', 'Daily Location Total']
 var loc = ['Seattle', '', 'Tokyoo', '', 'Dubai', '', 'Lima', '', 'Paris', '', 'Total'];
 var j = 0;
@@ -28,11 +28,14 @@ var data = document.createElement('article');
 parentElement.appendChild(data);
 var table = document.createElement('table');
 data.appendChild(table);
+var fillheader=function(){
 for (let hour = 0; hour <= hourarray.length; hour++) {
   var hourheader = document.createElement('th');
   hourheader.textContent = hourarray[hour];
   table.appendChild(hourheader);
-}
+}}
+
+var filltable=function(){
 for (i = 0; i <= citynamearray.length; i++) {
   var tablerow = document.createElement('tr');
   table.appendChild(tablerow);
@@ -50,32 +53,35 @@ for (i = 0; i <= citynamearray.length; i++) {
   s++;
   cellindex.textContent = total;
 }
+}
+fillheader();
+filltable();
 ///////lab09
 /////////////
 var addnewLocation = document.getElementById('addnewlocation');
 
-addnewLocation.addEventListener('submit', function (event){
+addnewLocation.addEventListener('submit', function (event) {
   event.preventDefult();
   var newlocationname = event.target.location.value;
   var newMin = event.target.Min.value;
   var newMax = event.target.Max.value;
   var newAvg = event.target.Avg.value;
-  var newlocation = new Store(newlocationname, newMin,newMax,newAvg);
+  var newlocation = new Store(newlocationname, newMin, newMax, newAvg);
   console.log(newavgTotal);
-    var tablenewrow = document.createElement('tr');
-    table.appendChild(tablenewrow);
-    var newtotal=0;
-    for (let j = 0; j <= hourarray.length; j++) {
-      var newavgranperhour = newlocation.avgRandom();
-      var cellindex = document.createElement('td');
-      cellindex.textContent = newavgranperhour;
-      newtotal += newavgranperhour;
-      console.log('total =' + newtotal);
+  newlocation.filltable();
+});
+  var tablenewrow = document.createElement('tr');
+  table.appendChild(tablenewrow);
+  var newtotal = 0;
+  for (let j = 0; j <= hourarray.length; j++) {
+    var newavgranperhour = newlocation.avgRandom();
+    var cellindex = document.createElement('td');
+    cellindex.textContent = newavgranperhour;
+    newtotal += newavgranperhour;
+    console.log('total =' + newtotal);
     cellindex.textContent = newtotal;
-  }});
-  consol.log(newlocationname.avgRandom());
-
-
+  }
+consol.log(newlocationname.avgRandom());
 console.log(addnewLocation());
 console.log(seattle);
 console.log(seattle.avgvalue);
